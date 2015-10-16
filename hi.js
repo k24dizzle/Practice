639bf94d94e95e97e156fee576d75042c6b64b4a
k24dizzle <zhaok24@uw.edu>
2015-10-16 10:53:33 -0700

    here we goooooo

---
+++ hi.js
@@ -0,0 +1,27 @@
+var map;
+function initMap() {
+	var seattle = {lat: 47.626353 , lng: -122.333144 };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: seattle,
+    zoom: 8
+  });
+}
+
+var clr = "red"
+function changeColor(element){
+ element.style.color=clr;
+  if(clr === "red"){
+    clr = "black";
+  }
+  else
+    clr = "red";
+}
+
+function popup(){
+	alert("SYKEEEEEE");
+}
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],
+	p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id))
+	{js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+	fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
